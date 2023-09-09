513ef7b3931a3db3eef20a334e4775cc25d9a8ae
altafbaba <altafbaba143s@gmail.com>
2023-09-09 19:57:37 +0530

    UI set

--- src/app/modules/maintenance/maintenance-card/maintenance-card.component.ts
+++ src/app/modules/maintenance/maintenance-card/maintenance-card.component.ts
@@ -20,6 +20,9 @@ export class MaintenanceCardComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.data);
     
+    
+  }
+  ngAfterContentInit(){
     this.MaintenanceServices.getDetailsmaintenance(this.data.vNumber).subscribe((res) => {
       this.MaintenanceData = res;
       console.log(this.MaintenanceData);
